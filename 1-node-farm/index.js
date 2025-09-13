e931ae1fdef3157364d0a37bc25235144a87c736
Rem Simiyu <remsimiyu@gmail.com>
2025-09-14 00:27:27 +0300

    Reading and writing files

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
 
+//Hello WORLD
 const hello = 'Hello World!';
 console.log(hello);
+
+//READING AND WRITING FILES
+//reading
+const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+console.log(textIn);
+
+//writing
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+fs.writeFileSync('./starter/txt/output.txt', textOut);
+console.log('File written!');
+
+
+
+
