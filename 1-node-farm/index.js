84ed10e38a822a96e47942c870d34c21f7f229ff
Rem Simiyu <remsimiyu@gmail.com>
2025-09-14 22:11:05 +0300

    Filling the templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -75,11 +75,24 @@ server.listen(8000, '127.0.0.1', () => {
 ////////////////////////BUILDING A (VERY) SIMPLE API////////////////////////
 
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
-const tempOverview = fs.readFileSync(`./starter/templates/template-overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html `, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html `, 'utf-8');
-
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace('/{%PRODUCTNAME%}/g', product.productName);
+    output = output.replace('/{%IMAGE%}/g', product.image);
+    output = output.replace('/{%PRICE%}/g', product.price);
+    output = output.replace('/{%FROM%}/g', product.from);
+    output = output.replace('/{%NUTRIENTS%}/g', product.nutrients);
+    output = output.replace('/{%QUANTITY%}/g', product.quantity);
+    output = output.replace('/{%DESCRIPTION%}/g', product.description);
+    output = output.replace('/{%ID%}/g', product.id);
+
+    if(!product.organic) output = output.replace('/{%NOT_ORGANIC%}/g', 'not-organic');
+    return output;
+}
 
 const server = http.createServer((req, res) => {
     const pathName =  req.url;
@@ -87,6 +100,10 @@ const server = http.createServer((req, res) => {
     //overview page
     if(pathName === '/' || pathName === '/overview') {
         res.writeHead(200, {'Content-type' : 'text/html'})
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+        console.log(cardsHtml);
+        
         res.end(tempOverview);
 
     //product page
