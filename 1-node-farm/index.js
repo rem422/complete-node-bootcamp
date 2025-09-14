88f96efb98c592d3b2abf840b682620e79d9a249
Rem Simiyu <remsimiyu@gmail.com>
2025-09-14 10:10:07 +0300

    Building a very simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -48,7 +48,6 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('Litsening to request on port 8000');
 });
- */ 
 
 ////////////////////////ROUTING////////////////////////
 
@@ -71,4 +70,35 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('Litsening to request on port 8000');
 });
+ */ 
+
+////////////////////////BUILDING A (VERY) SIMPLE API////////////////////////
+
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
+    const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName =  req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+            res.writeHead(200, {'Content-type' : 'application/json'});
+            res.end(data);
+    } else {
+        res.writeHead(404), {
+            'Content-type' : 'text/html',
+            'my-own-header' : 'hello-world'
+        };
+        res.end(`<h1>Page not found!</h1>`);
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Litsening to request on port 8000');
+});
+
+
 
