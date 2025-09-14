62867d3b2582742990e9df6b8fc3f14363a942e3
Rem Simiyu <remsimiyu@gmail.com>
2025-09-14 22:29:00 +0300

    Filling the templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -81,16 +81,16 @@ const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.h
 const dataObj = JSON.parse(data);
 
 const replaceTemplate = (temp, product) => {
-    let output = temp.replace('/{%PRODUCTNAME%}/g', product.productName);
-    output = output.replace('/{%IMAGE%}/g', product.image);
-    output = output.replace('/{%PRICE%}/g', product.price);
-    output = output.replace('/{%FROM%}/g', product.from);
-    output = output.replace('/{%NUTRIENTS%}/g', product.nutrients);
-    output = output.replace('/{%QUANTITY%}/g', product.quantity);
-    output = output.replace('/{%DESCRIPTION%}/g', product.description);
-    output = output.replace('/{%ID%}/g', product.id);
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
 
-    if(!product.organic) output = output.replace('/{%NOT_ORGANIC%}/g', 'not-organic');
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     return output;
 }
 
@@ -101,10 +101,10 @@ const server = http.createServer((req, res) => {
     if(pathName === '/' || pathName === '/overview') {
         res.writeHead(200, {'Content-type' : 'text/html'})
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
-        console.log(cardsHtml);
-        
-        res.end(tempOverview);
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
 
     //product page
     } else if (pathName === '/product') {
