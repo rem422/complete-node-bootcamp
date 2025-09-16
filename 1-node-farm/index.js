f7d6ec8b996d6cd3d6213f4d0da4c52c32a16c67
Rem Simiyu <remsimiyu@gmail.com>
2025-09-17 01:08:07 +0300

    Our Own modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('../modules/replaceTemplate');
 
 /* 
 /////////////////////////////Hello WORLD/////////////////////////////
@@ -80,20 +81,6 @@ const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-pro
 const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true);
     
@@ -130,7 +117,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Litsening to request on port 8000');
+    console.log('Litsening to request on port http://localhost:8000');
 });
 
 
