8b625a512251eb27fabfb445b5747ac908a5f5d8
Rem Simiyu <remsimiyu@gmail.com>
2025-09-16 13:31:51 +0300

    Changed the localhost

--- 1-node-farm/final/index.js
+++ 1-node-farm/final/index.js
@@ -92,5 +92,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port http://localhost:8000');
 });
