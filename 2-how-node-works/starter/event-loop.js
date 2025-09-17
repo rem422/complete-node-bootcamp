e1780dc62fba63f07ba96502bf5606e50f56f126
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-17 19:50:00 +0300

    The event loop in practice

---
+++ 2-how-node-works/starter/event-loop.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 3;
+
+setTimeout(() => console.log('timer 1 finished'), 0);
+setImmediate(() => console.log('Immidate 1 finished'));
+
+fs.readFile('test-file.txt', 'utf-8', () => {
+    console.log('-----------------------');
+
+    setTimeout(() => console.log('timer 2 finished'), 0);
+    setTimeout(() => console.log('timer 3 finished'), 3000);
+    setImmediate(() => console.log('Immidate 2 finished'));
+
+    process.nextTick(() => console.log('Process.nextTick'));
+
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,'Password encrypted');
+    });
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,'Password encrypted');
+    });
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,'Password encrypted');
+    });
+    crypto.pbkdf2('password', 'salt', 10000, 1024, 'sha512', () => {
+        console.log(Date.now() - start,'Password encrypted');
+    });
+});
+
+console.log('Hello from the top-level code');
