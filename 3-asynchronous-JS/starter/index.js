3e286a55e04ef9381a2c7dddc3ceeffb7e5e5597
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-18 13:14:24 +0300

    From callback hell to promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,17 +1,38 @@
 const fs = require('fs');
 const superagent = require('superagent');
+
+//callback
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//     console.log(`Breed: ${data}`);
+//
+//     superagent
+//         .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//         .end((err, res) => {
+//             if(err) return console.log(err.message);
+//             console.log(res.body.message);
+//
+//         fs.writeFile('dog-image.txt', res.body.message, (err) => {
+//             if(err) return console.log(err.message);
+//             console.log('Random dog image saved to file!');
+//         });
+//     });
+// });
+
+//Promises
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
     superagent
         .get(`https://dog.ceo/api/breed/${data}/images/random`)
-        .end((err, res) => {
-            if(err) return console.log(err.message);
+        .then(res => {
             console.log(res.body.message);
 
-        fs.writeFile('dog-image.txt', res.body.message, (err) => {
-            if(err) return console.log(err.message);
-            console.log('Random dog image saved to file!');
-        });
+            fs.writeFile('dog-image.txt', res.body.message, (err) => {
+                if(err) return console.log(err.message);
+                console.log('Random dog image saved to file!');
+            });
+        })
+        .catch (err => {
+            console.log(err.message);
     });
 });
