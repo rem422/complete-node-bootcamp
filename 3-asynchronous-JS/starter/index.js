4ced41db11439dd006a20c86401e4b860abb7f2d
Rem Simiyu <84072712+rem422@users.noreply.github.com>
2025-09-18 13:56:10 +0300

    Consuming promises with async/await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -37,18 +37,34 @@ const writeFilePro = (file, data) => {
     });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-    .then(data => {
+// readFilePro(`${__dirname}/dog.txt`)
+//     .then(data => {
+//         console.log(`Breed: ${data}`);
+//         return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     })
+//     .then(res => {
+//         console.log(res.body.message);
+//         return writeFilePro('dog-image.txt', res.body.message)
+//     })
+//     .then(() => {
+//         console.log('Random dog image saved to file!');
+//     })
+//     .catch (err => {
+//         console.log(err);
+//     });
+
+//Async/ await
+const getDogPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
         console.log(`Breed: ${data}`);
-        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-    })
-    .then(res => {
+
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
         console.log(res.body.message);
-        return writeFilePro('dog-image.txt', res.body.message)
-    })
-    .then(() => {
+        await writeFilePro('dog-image.txt', res.body.message);
         console.log('Random dog image saved to file!');
-    })
-    .catch (err => {
+    } catch(err) {
         console.log(err);
-    });
+    }
+};
+getDogPic();
